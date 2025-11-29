18c123dd26c837b3b0c78f123672322fa1269284
ichiro-kazusa <ichiro.kazusa@gmail.com>
2025-11-29 23:05:53 +0900

    Add version entries v0.0.1 to v0.0.6
    Added new version entries to the DOC_VERSIONS array.

--- versions.js
+++ versions.js
@@ -1,6 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "dev",
+  "v0.0.6",
+  "v0.0.5",
+  "v0.0.4",
+  "v0.0.3",
+  "v0.0.2",
+  "v0.0.1",
 ];
 var DOCUMENTER_NEWEST = "v0.0.7";
 var DOCUMENTER_STABLE = "stable";
